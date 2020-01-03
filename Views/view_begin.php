76366200fbd635ea77d8d2ff2cb68381c0120aa5
David-Charlesia <pro.charlesia.david@outlook.com>
2020-01-03 11:05:38 +0100

    vue begin started

---
+++ Views/view_begin.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>WYES PROD</title>
+  </head>
+  <body>
+    <header>
+      <a></a>
+      <img src="Content/img/logo-wyes.jpg" alt="WYES">
+      <div class="">
+
+      </div>
+    </header>
