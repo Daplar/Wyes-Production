c252a536152d6c83d49497fb21c0d0178afcdc6d
David-Charlesia <pro.charlesia.david@outlook.com>
2020-01-06 13:00:48 +0100

    création index.php

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
 <?php
 
+require_once "Models/Model.php";
+require_once "Controllers/Controller.php";
+
+$controllers=["home"];
+$controllers_default="home";
+
+if(isset($_GET['controller']) and in_array($_GET['controller'],$controllers))
+{
+  $nom_controller=$_GET['controller'];
+}else{
+  $nom_controller=$controllers_default;
+}
+
+$nom_classe='Controller_'.$nom_controller;
+$nom_fichier='Controllers/'.$nom_classe.'.php';
+
+if(file_exist($nom_fichier)){
+  include_once $nom_fichier;
+  $controller=new $nom_classe();
+}else{
+  exit("Error 404: not found.");
+}
  ?>
