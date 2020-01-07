a2e85c1dcb05ed824e9ae0e2af9470cd974415de
David-Charlesia <pro.charlesia.david@outlook.com>
2020-01-07 11:10:29 +0100

    viewhome

--- Views/view_home.php
+++ Views/view_home.php
@@ -1,4 +1,9 @@
-<?php
-require('view_begin.php');
-?>
+<?php require('view_begin.php');?>
 <h1>Test view home</h1>
+<div class="overview">
+  <table>
+    <?php foreach ($tab as $cle => $ligne): ?>
+      <tr><td><?=$cle?></td><td><?=$ligne?></td></tr>
+  </table>
+</div>
+<?php require('view_end.php');?>
