b31126f06fb53346bc8720225958feafb9ee198e
David-Charlesia <pro.charlesia.david@outlook.com>
2020-01-07 11:56:35 +0100

    modifs css, modifs view begin & end

--- Views/view_end.php
+++ Views/view_end.php
@@ -1,2 +1,14 @@
+<div id="end">
+  <div class="contact">
+    <p>Mail : maxime@wyes.fr</p>
+    <p>Adresse : 37, Quai de Grenelle 75015 Paris</p>
+    <p>Telephone : 0000-00-00-00</p>
+  </div>
+  <div class="links">
+    <a href="http://wyes.fr/">Site officiel</a>
+    <a href="https://www.facebook.com/teamwyes/">Notre Facebook</a>
+  </div>
+</div>
+</div>
 </body>
 </html>
