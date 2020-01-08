10e43725d2cee360ffc3b2a30d5e22ad6397940c
pln <nohetpl@gmail.com>
2020-01-08 15:55:45 +0100

    test4

--- Views/view_begin.php
+++ Views/view_begin.php
@@ -19,31 +19,36 @@
           <a href="http://www.instagram.com"><img src="Content/img/logo_instagram.png" alt="Instagram"></a>
         </div>
       </header>
-      <nav>
-        <ul>
-          <li><a href="?controller=home">Accueil</a></li>
-          <li><a href="">Suivi produit</a></li>
-          <li><a href="">Gestion retour</a></li>
-          <li><a href="?controller=production&action=overview">Production</a></li>
-          <li><a href="">Contact</a></li>
-        </ul>
-      </nav>
+    <nav>
+      <ul>
+        <li><a href="?controller=home">Accueil</a></li>
+        <li><a href="">Suivi produit</a></li>
+        <li><a href="">Gestion retour</a></li>
+        <li><a href="?controller=production&action=overview">Production</a></li>
+        <li><a href="">Contact</a></li>
+      </ul>
+    </nav>
 
-      <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js">
-      var liste=document.querySelector('nav ul');
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js">
+    var liste=document.querySelector('nav ul');
 
-      var position_top_raccourci = liste.offset().top;
+    var position_top_raccourci = liste.offset().top;
 
-      $(window).scroll(function(){
-        if($(this).scrollTop() > position_top_raccourci)
-        {
-          liste.addClass("fixNavigation");
-        }else{
-          liste.removeClass("fixNavigation");
-        }})
+    $(window).scroll(function(){
+      if($(this).scrollTop() > position_top_raccourci)
+      {
+        liste.addClass("fixNavigation");
+      }else{
+        liste.removeClass("fixNavigation");
+      }})
 
-      </script>
-      <div class="connexion">
-        <a href="">Se connecter</a>
-        <a href="">Créer un compte</a>
-      </div>
+    </script>
+    <div class="connexion">
+      <a href="">Se connecter</a>
+      <a href="">Créer un compte</a>
+    </div>
+
+    <div class="overview">
+      <h2>Overview</h2>
+      <p>Example</p>
+    </div>
