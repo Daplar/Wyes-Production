02e346665efc978498b2d0b4fd65cb0b388d26f2
VARRAT Alwis <varrat.asanka@gmail.com>
2020-01-09 15:53:30 +0100

    Vérification des output controller_user.php

--- Controllers/Controller_user.php
+++ Controllers/Controller_user.php
@@ -8,18 +8,28 @@ class Controller_user extends Controller
 
   public function verification_create($infos){
     //echo ('dans verif');
-   if( (! isset($infos['name'])) or ($infos['name'] == '') ){
+
+   if( (!isset($infos['name'])) or !preg_match("/^([a-zA-Z' ]+)$/",$infos["name"])){
      //echo('pas de qté');
      $this->render('message',
        ['title' => "",
        'message' => "Le nom n'est pas renseigné, est une chaîne vide ou que des espaces, veuillez réesayez."]);
    }
 
-   if( (! isset($infos['e_mail'])) or ($infos['e_mail'] == '') or preg_match('#^ *$#', $infos['e_mail'])){
+   $pw='/^(?=.*\d)(?=.*[A-Za-z])[0-9A-Za-z!@#$%]{8,12}$/';
+   $pattern = "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,})$/i";
+
+   if( (! isset($infos['e_mail'])) or !preg_match($pattern, $infos['e_mail'])){
 			 $this->render('message',
 				 ['title' => "",
 				 'message' => "Veuillez entrez une adresse e_mail valide."]);
 		 }
+
+     if( (! isset($infos['password'])) or !preg_match($pw, $infos['password'])){
+  			 $this->render('message',
+  				 ['title' => "",
+  				 'message' => "Veuillez entrez un mot de passe valide."]);
+  		 }
    }
 
    public function action_form_connection(){
