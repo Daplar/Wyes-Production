5cd84f44b64a91e20af65a733fc3e96c26696eb1
David-Charlesia <pro.charlesia.david@outlook.com>
2020-01-09 15:10:58 +0100

    ajout d'une image

--- Views/view_home.php
+++ Views/view_home.php
@@ -1,5 +1,6 @@
 <?php require('view_begin.php');?>
-<h1>Test view home</h1>
+<h1>Page d'acceuil</h1>
+<img src="Content/img/hqdefault.jpg" alt="Lunette WYES">
 <div class="overview">
   <p> il y a actuellement <?php echo $nb_lunettes?> produits dans la base de données </p>
 </div>
